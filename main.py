7520799fef2f6f2df18a43f4c683969da3e3319d
taylorsreid <taylorsreid@gmail.com>
2022-05-15 05:39:40 -1000

    Better error handling and colors.

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ try:
     print("MySQL Connector imported successfully...")
 except:
     print("Failed to import MySQL Connector!  You must run")
-    print("\u001b[31m pip install mysql-connector-python")
-    print("\u001b[0m in command prompt / terminal to use this application.")
+    print("\t\u001b[32m pip install mysql-connector-python")
+    print("\u001b[0m in command prompt or terminal to use this application.")
 
-#Locations & Credentials
-mydb = mysql.connector.connect(
-    host = "", #STORE IN A SEPARATE FILE THAT IS GITIGNORED
-    user = "", #STORE IN A SEPARATE FILE THAT IS GITIGNORED
-    password = "" #STORE IN A SEPARATE FILE THAT IS GITIGNORED
-)
+#
+try:
+    #Locations & Credentials
+    mydb = mysql.connector.connect(
+        host = "", #STORE IN A SEPARATE FILE THAT IS GITIGNORED
+        user = "", #STORE IN A SEPARATE FILE THAT IS GITIGNORED
+        password = "" #STORE IN A SEPARATE FILE THAT IS GITIGNORED
+    )
+    #cursor object declaration
+    mycursor = mydb.cursor()
+    print("Successfully connected to MySQL database...")
 
-#TEST CODE
-mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-  print(x)
+    #BEGIN TEST CODE
+    print("\nList of databases:")
+    mycursor.execute("SHOW DATABASES")
+    for x in mycursor:
+        print(f"\t {x}")
+    print()
+    #END TEST CODE
+
+except:
+    print("Error connecting to MySQL database!  Please check your connection credentials and try again!")
+
